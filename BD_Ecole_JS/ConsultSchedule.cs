ead010f03435743b8618b4a44718ab48b142c9bf
samuel <samueljames.2003@hotmail.com>
2023-04-14 13:22:46 +0200

    Remove usings

--- BD_Ecole_JS/ConsultSchedule.cs
+++ BD_Ecole_JS/ConsultSchedule.cs
@@ -2,13 +2,8 @@
 using Projet_BDEcole.Gestion;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Configuration;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BD_Ecole_JS
